8143b68e43c48022500ae8a731cb225d2f2ac21b
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2019-09-19 14:07:27 +0100

    function arrow changed

--- greet.js
+++ greet.js
@@ -1,3 +1,5 @@
-const helloWorld = (name = "world") => {
-  return `Hello ${name}!`;
-}
+// const helloWorld = (name = "world") => {
+//   return `Hello ${name}!`;
+// }
+
+const helloWorld = (name = "World") => {`Hello ${name}!`};
